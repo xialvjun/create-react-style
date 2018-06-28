4e8122d90cf5c91e704c57e9bb8f58411dd00c53
xialvjun <xialvjun@live.com>
2018-06-28 22:45:26 +0800

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { Fragment, Component, ReactNode, ComponentClass } from "react";
 
 import { createStyle } from "./createStyle";
 
+export * from "./createStyle";
+
 export const Style: ReturnType<typeof createStyle> = createStyle();
-export { createStyle };
